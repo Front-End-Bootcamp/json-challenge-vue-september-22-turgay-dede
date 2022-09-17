14dd21c64e608ea0fd2690527fd71cbfc54aa15a
turgay-dede <turgay.dede28@hotmail.com>
2022-09-17 09:43:47 +0300

    refactoring

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,217 +1,5 @@
-const data = [
-	{
-		"name": "Abdulkadir Çelebi",
-		"id": "631db8d58b3d1cc9a2144cf5",
-		"group": "Orchid",
-		"type": null
-	},
-	{
-		"name": "Abdullah Kuş",
-		"id": "631db8d5e9d3a5f66526a610",
-		"group": "SteelBlue",
-		"type": null
-	},
-	{
-		"name": "Ali̇can Berat Tutumoğlu",
-		"id": "631db8d5ccd6352cc1714fe1",
-		"group": "Orchid",
-		"type": null
-	},
-	{
-		"name": "Alihan Değirmenciler",
-		"id": "631db8d5f2f01a8fa3a23a21",
-		"group": "SteelBlue",
-		"type": null
-	},
-	{
-		"name": "Altuğ Altay",
-		"id": "631db8d59fc2797f8369ce4b",
-		"group": "IndianRed",
-		"type": null
-	},
-	{
-		"name": "Anıl Sezer",
-		"id": "631db8d5e4bd98e9a2934be8",
-		"group": "SteelBlue",
-		"type": null
-	},
-	{
-		"name": "Aybüke Polat",
-		"id": "631db8d551b762e7e9a92f75",
-		"group": "SteelBlue",
-		"type": null
-	},
-	{
-		"name": "Emircan Demir",
-		"id": "631db8d5f3c680c55c6cdb0a",
-		"group": "Orchid",
-		"type": null
-	},
-	{
-		"name": "Emircan Özenir",
-		"id": "631db8d51f69ce4d2b6e43ea",
-		"group": "YellowGreen",
-		"type": null
-	},
-	{
-		"name": "Engin İnsel",
-		"id": "631db8d566fd25d2034e2f26",
-		"group": "Orchid",
-		"type": null
-	},
-	{
-		"name": "Esra Koçak",
-		"id": "631db8d5164158e25610e4c1",
-		"group": "IndianRed",
-		"type": null
-	},
-	{
-		"name": "Fatih Tarım",
-		"id": "631db8d5ce015ffe1b934ade",
-		"group": "SteelBlue",
-		"type": null
-	},
-	{
-		"name": "Fatih Tufan",
-		"id": "631db8d5e23e63fd79581954",
-		"group": "SteelBlue",
-		"type": null
-	},
-	{
-		"name": "Feruze Beyza Başer",
-		"id": "631db8d5ce659b4ea9500f13",
-		"group": "SteelBlue",
-		"type": null
-	},
-	{
-		"name": "Furkan Akin",
-		"id": "631db8d51b06709289c62227",
-		"group": "LightSeaGreen",
-		"type": null
-	},
-	{
-		"name": "Gorkem Sakalak",
-		"id": "631db8d5d1bfff434fffbe16",
-		"group": "YellowGreen",
-		"type": null
-	},
-	{
-		"name": "İlker Durmaz",
-		"id": "631db8d5afc25569fabd9cfb",
-		"group": "SteelBlue",
-		"type": null
-	},
-	{
-		"name": "Irina Ivanova",
-		"id": "631db8d5c30b2de5a8096e81",
-		"group": "Orchid",
-		"type": null
-	},
-	{
-		"name": "İsa Sarı",
-		"id": "631db8d5a14b0c131b273d39",
-		"group": "IndianRed",
-		"type": null
-	},
-	{
-		"name": "İsmet Erkin Yavuzlar",
-		"id": "631db8d538130a4d8379e876",
-		"group": "IndianRed",
-		"type": null
-	},
-	{
-		"name": "Kaplan Arkan",
-		"id": "631db8d52e9d315ff36d861c",
-		"group": "IndianRed",
-		"type": null
-	},
-	{
-		"name": "Mahmut Çapkın",
-		"id": "631db8d5fd326c55b14e0041",
-		"group": "Orchid",
-		"type": null
-	},
-	{
-		"name": "Mahmut Hüdai Göl",
-		"id": "631db8d503b36479e39f3e90",
-		"group": "Orchid",
-		"type": null
-	},
-	{
-		"name": "Mehmet Kırsakal",
-		"id": "631db8d5c418ddb40198d2c3",
-		"group": "SteelBlue",
-		"type": null
-	},
-	{
-		"name": "Onur Çöpoğlu",
-		"id": "631db8d530d1dafd77aec3aa",
-		"group": "YellowGreen",
-		"type": null
-	},
-	{
-		"name": "Recep Emre Gürdrama",
-		"id": "631db8d50c0138167e74672b",
-		"group": "IndianRed",
-		"type": null
-	},
-	{
-		"name": "Samet Keleş",
-		"id": "631db8d5d02e4c9d6b2ea90a",
-		"group": "Orchid",
-		"type": null
-	},
-	{
-		"name": "Seyit Arslantürk",
-		"id": "631db8d56c23487eb3b5a341",
-		"group": "Orchid",
-		"type": null
-	},
-	{
-		"name": "Vahide Erdoğan",
-		"id": "631db8d5d8ec17dbf10f7055",
-		"group": "IndianRed",
-		"type": null
-	},
-	{
-		"name": "Yunus Emre Topçu",
-		"id": "631db8d59ff3c7023cf7ac1a",
-		"group": "SteelBlue",
-		"type": null
-	},
-	{
-		"name": "Hüseyin Can Çirkin",
-		"id": "631db8d5951bff669092e4a5",
-		"group": "LightSeaGreen",
-		"type": "assistant"
-	},
-	{
-		"name": "Eralp Özcan",
-		"id": "631db8d55e3d014e2087b9ac",
-		"group": "IndianRed",
-		"type": "assistant"
-	},
-	{
-		"name": "Turgay Dede",
-		"id": "631db8d5f2338e141e07dc76",
-		"group": "YellowGreen",
-		"type": "assistant"
-	},
-	{
-		"name": "Osman Mahmutçepoğlu",
-		"id": "631db8d5d05cf94db3255dd8",
-		"group": "Orchid",
-		"type": "assistant"
-	},
-	{
-		"name": "Enes Taha Sarı",
-		"id": "631db8d560cfc88b4a6aa200",
-		"group": "SteelBlue",
-		"type": "assistant"
-	}
-]
+import data from "../data/data.json"
 
-let assistants = data.filter(d => d.type != null).map(a => a.name)
 let groups = []
 
 data.map(d => {
@@ -220,19 +8,19 @@ data.map(d => {
 	}
 })
 
-function getStudentsForGroupName(groupName) {
-	let students = data.filter(d => d.group === groupName).map(s => s.name)
+function getStudentsForGroupName(groupName, data) {
+	let students = data.filter(d => d.group === groupName && d.type == null).map(s => s.name)
 	return students;
 }
 
-function getAssistantForGroupName(groupName) {
+function getAssistantForGroupName(groupName ,data) {
 	let assistant = data.filter(d => d.group === groupName).filter(s => s.type != null).map(a => a.name)
 	return assistant;
 }
 
-function createResult(groupName) {
-	let students = getStudentsForGroupName(groupName)
-	let assistant = getAssistantForGroupName(groupName)
+function createResult(groupName, data) {
+	let students = getStudentsForGroupName(groupName, data)
+	let assistant = getAssistantForGroupName(groupName, data)
 	let result = {
 		"groupName": groupName,
 		"students": students,
@@ -240,5 +28,5 @@ function createResult(groupName) {
 	}
 	return result;
 }
-let result = groups.map(g => createResult(g))
+let result = groups.map(g => createResult(g, data))
 console.log(result);
